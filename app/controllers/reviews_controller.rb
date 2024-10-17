09b0e018c06d647f8c6924819559ba982b1e656a
Eric Todasco <erictodasco@me.com>
2024-10-17 15:31:54 -0400

    Rails Advanced Routing

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -3,4 +3,19 @@ class ReviewsController < ApplicationController
         @review = Review.new
         @restaurant = Restaurant.find(params[:restaurant_id])
     end
+
+    def create
+        @review = Review.new(review_params)
+        @restaurant = Restaurant.find(params[:restaurant_id])
+        @review.Restaurant = @restaurant
+        @review.save
+        redirect_to restaurant_path(@restaurant)
+    end
+
+    private
+
+    def review_params
+        params.require(:review).permit(:content)
+    end
+
 end
