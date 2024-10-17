944764d9bc52cbd5c00e552c54d7ec3af10f9a26
Eric Todasco <erictodasco@me.com>
2024-10-17 08:46:19 -0400

    Rails Advanced Routing

--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,6 @@ Rails.application.routes.draw do
   get "restaurants", to: "restaurants#index"
   get "restaurants/new", to: "restaurants#new"
   post "restaurants", to: "restaurants#create"
-  get "restaurants/:id", to: "restaurants#show"
+  get "restaurants/:id", to: "restaurants#show", as: :restaurant
 
 end
