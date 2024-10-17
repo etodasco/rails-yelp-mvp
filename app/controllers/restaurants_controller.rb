4f6b2ac584d7b3848b52eb5385caf91b99b75084
Eric Todasco <erictodasco@me.com>
2024-10-17 12:07:47 -0400

    Rails Advanced Routing

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -33,7 +33,7 @@ class RestaurantsController < ApplicationController
   private
 
   def restaurant_params
-    params.require(:restaurant).permit(:name, :address, :category)
+    params.require(:restaurant).permit(:name, :address, :phone_number, :category)
   end
 
   def set_restaurant
