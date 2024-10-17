c0c226aad119a80176641fdf2ceecf5c9aeff23f
Eric Todasco <erictodasco@me.com>
2024-10-17 11:18:13 -0400

    Rails Advanced Routing

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,2 +1,7 @@
 class Restaurant < ApplicationRecord
+  
+  CATEGORY = ["chinese", "italian", "japanese", "french", "belgian"]
+
+  value = gets.chomp
+  validates :name, :phone_number, :address, :category, inclusion: { in: CATEGORY, message: "#{value} is not a valid category" }
 end
