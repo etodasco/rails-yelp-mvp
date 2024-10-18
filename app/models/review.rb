3710a8a1626bede8980e3584cd8e8ab7578fefe7
Eric Todasco <erictodasco@me.com>
2024-10-17 20:10:03 -0400

    Rails Advanced Routing

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,5 +1,6 @@
 class Review < ApplicationRecord
   belongs_to :Restaurant
 
-  validates :content, presence: true
+  validates :content, :rating, presence: true
+  validates :rating, inclusion: { in: 1..5 }
 end
